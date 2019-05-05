a3178fe669aacb0c8c3c300b7fdcfcc4330510ed
Atsuyoshi-N <ash.sit.you7@ezweb.ne.jp>
2019-05-05 13:04:21 +0900

    imprement switch locale button

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "posts#index"
-  resources :posts
+  scope '(:locale)', locale: /#{I18n.available_locales.map(&:to_s).join('|')}/ do
+    root "posts#index"
+    resources :posts
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
