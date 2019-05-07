31feb3591ee9ffe13d5cdbcfb27fb2874acd72c5
Atsuyoshi-N <ash.sit.you7@ezweb.ne.jp>
2019-05-08 01:17:23 +0900

    work i18n-js with webpack

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -1,7 +1,9 @@
 import React from "react"
 import PropTypes from "prop-types"
+import I18n from 'i18n-js/index.js.erb'
 class HelloWorld extends React.Component {
   render () {
+    console.log(I18n.t('hello'));
     return (
       <React.Fragment>
         Greating: {this.props.greating}
